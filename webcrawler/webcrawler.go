45f1022b1b4db43c2a1cb0773082514de01ae20d
Zoltan <zotheconqueror@googlegroups.com>
2021-08-05 20:19:54 +0200

    Solution to create Web Crawler fetch Urls simultaneously. (The idea is based on several other solution that already existed in this time)

---
+++ webcrawler/webcrawler.go
@@ -0,0 +1,132 @@
+/*
+A Tour of Go
+
+Exercise: Web Crawler
+In this exercise you'll use Go's concurrency features to parallelize a web crawler.
+
+Modify the Crawl function to fetch URLs in parallel without fetching the same URL twice.
+
+Hint: you can keep a cache of the URLs that have been fetched on a map, but maps alone are not safe for concurrent use!
+
+https://tour.golang.org/concurrency/10
+*/
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type SafeUrlFetchCounter struct {
+	mu sync.Mutex
+	urls map[string]bool
+	wg sync.WaitGroup
+}
+
+func (c *SafeUrlFetchCounter) IsFetched(key string) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	v, ok := c.urls[key]
+	if ok {
+		return v
+	} else {
+		return false
+	}
+}
+
+func (c *SafeUrlFetchCounter) SetFetched(key string) {
+	c.mu.Lock()
+	c.urls[key] = true
+	c.mu.Unlock()
+}
+
+type Fetcher interface {
+	// Fetch returns the body of URL and
+	// a slice of URLs found on that page.
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+var fetched SafeUrlFetchCounter = SafeUrlFetchCounter{urls: make(map[string]bool)}
+
+// Crawl uses fetcher to recursively crawl
+// pages starting with url, to a maximum of depth.
+func Crawl(url string, depth int, fetcher Fetcher) {
+	// TODO: Fetch URLs in parallel.
+	// TODO: Don't fetch the same URL twice.
+	// This implementation doesn't do either:
+	defer fetched.wg.Done()
+	if depth <= 0 {
+		return
+	}
+	if fetched.IsFetched(url) {
+		return
+	}
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+	for _, u := range urls {
+		fetched.wg.Add(1)
+		go Crawl(u, depth-1, fetcher)
+	}
+	fetched.SetFetched(url)
+	return
+}
+
+func main() {
+	fetched.wg.Add(1)
+	Crawl("https://golang.org/", 4, fetcher)
+	fetched.wg.Wait()
+}
+
+// fakeFetcher is Fetcher that returns canned results.
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+// fetcher is a populated fakeFetcher.
+var fetcher = fakeFetcher{
+	"https://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"https://golang.org/pkg/",
+			"https://golang.org/cmd/",
+		},
+	},
+	"https://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/cmd/",
+			"https://golang.org/pkg/fmt/",
+			"https://golang.org/pkg/os/",
+		},
+	},
+	"https://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+	"https://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+}
